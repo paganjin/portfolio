6ea33f7cad90b531c8291840c1439ed7f504a7bb
Tianyou JIN <ohjinjinjin@gmail.com>
2021-10-23 14:13:43 +1100

    Updates

---
+++ precache-manifest.d83776be64ecc6c72ab45906df6dd210.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11004de087242150f7478a8653b40a3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2692fc5686dc2a3e7d66",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "fd489cced9c4ce4b708a",
+    "url": "/portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "2692fc5686dc2a3e7d66",
+    "url": "/portfolio/static/js/2.60118682.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.60118682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd489cced9c4ce4b708a",
+    "url": "/portfolio/static/js/main.228e7f72.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
